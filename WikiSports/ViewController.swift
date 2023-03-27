533f91b915f4ae8dfd9541825d6c1fe081eff4ac
Franklin Gaspar <franklin.gaspar@hotmail.com>
2023-03-27 07:36:34 -0300

    first commit

---
+++ WikiSports/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WikiSports
+//
+//  Created by franklin gaspar on 27/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+ 
+
+}
+
